a2e167f7845f5fe4c7281cfc8c1533a651dde22c
LdsFish <ldsfish0307@gmail.com>
2017-10-26 22:12:24 +0800

    finish

--- test/getDetails.js
+++ test/getDetails.js
@@ -1,18 +1,20 @@
 /* eslint-disable no-undef */
 var expect = require('chai').expect
 
-describe('getDetails', function() {
+describe('getDetails', function () {
     var getDetails = require('../lib/getDetails')
 
-    describe('address', function() {
-        it('getDetails.address()', function() {
-            expect(getDetails.Address()).to.equal('')
+    describe('金門大學', function () {
+        it("getDetails.address('highWall')).to.equal('金大')", function () {
+            expect(getDetails.address('highWall')).to.equal('金大')
         })
-    })
 
-    describe('phone', function() {
-        it('getDetails.phone()', function() {
-            expect(getDetails.phone()).to.equal('')
+        it("getDetails.phone('highWall')).to.equal('0987487987')", function () {
+            expect(getDetails.phone('highWall')).to.equal('0987487987')
+        })
+
+        it("getDetails.open_hours('highWall')).to.equal('0~24')", function () {
+            expect(getDetails.open_hours('highWall')).to.equal('0~24')
         })
     })
 })
